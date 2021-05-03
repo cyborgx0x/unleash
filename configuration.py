dbb0a0f91a68fed02958666cd254f9977abc9c2c
cyborgx0x <leeboykt@gmail.com>
2021-05-04 05:51:00 +0700

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://s797lpai7wm3gucu:td9qh8rdi1e87jly@xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ujmuqrlhu79bpuef'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('JAWSDB_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     
