5f2d538349a178147990a169a7a250c041547a66
Nguyễn Khánh Thuận <leeboykt@gmail.com>
2023-09-20 23:38:29 +0700

    configure

--- app/configuration.py
+++ app/configuration.py
@@ -1,17 +1,23 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-DB_USERNAME = os.environ.get('DB_USERNAME', 'root')
-DB_PASSWORD = os.environ.get('DB_PASSWORD', '123456')
-DB_HOST = os.environ.get('DB_HOST', 'db')
-DB_PORT = os.environ.get('DB_PORT', '5432')
-DB_TABLE = os.environ.get('DB_TABLE', 'unleash')
+DB_USERNAME = os.environ.get("DB_USERNAME", "root")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "123456")
+DB_HOST = os.environ.get("DB_HOST", "db")
+DB_PORT = os.environ.get("DB_PORT", "5432")
+DB_TABLE = os.environ.get("DB_TABLE", "unleash")
+
+if DB_USERNAME == None:
+    q = "sqlite:////tmp/test.db"
+else:
+    q = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_TABLE}"
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_TABLE}'
+    SQLALCHEMY_DATABASE_URI = q
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
