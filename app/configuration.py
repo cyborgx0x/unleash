e7836ebfdd3a050462a31cfe864567e481d9e016
Nguyễn Khánh Thuận <leeboykt@gmail.com>
2023-09-21 00:06:38 +0700

    change config

--- app/configuration.py
+++ app/configuration.py
@@ -12,12 +12,12 @@ DB_PORT = os.environ.get("DB_PORT", "5432")
 DB_TABLE = os.environ.get("DB_TABLE", "unleash")
 
 if DB_USERNAME == "root":
-    q = "sqlite:////unleash.db"
+    q = "sqlite:///unleash.db"
 else:
     q = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_TABLE}"
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = "sqlite:////unleash.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///unleash.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
