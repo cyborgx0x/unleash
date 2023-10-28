b60b431b3a406d4b311f7e4b1cea36cd11f58a58
Lucas Ng <leeboykt@gmail.com>
2023-10-28 11:15:15 +0700

    add env

--- app/configuration.py
+++ app/configuration.py
@@ -5,19 +5,17 @@ from dotenv import load_dotenv
 load_dotenv()
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-DB_USERNAME = os.environ.get("DB_USERNAME", "root")
-DB_PASSWORD = os.environ.get("DB_PASSWORD", "123456")
-DB_HOST = os.environ.get("DB_HOST", "db")
-DB_PORT = os.environ.get("DB_PORT", "5432")
-DB_TABLE = os.environ.get("DB_TABLE", "unleash")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
 
-if DB_USERNAME == "root":
-    q = "sqlite:///unleash.db"
+if os.environ.get("DB") == "POSTGRES":
+    q = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/POSTGRES_DB"
 else:
-    q = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_TABLE}"
+    q = "sqlite:///unleash.db"
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = "sqlite:///unleash.db"
+    SQLALCHEMY_DATABASE_URI = q
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
